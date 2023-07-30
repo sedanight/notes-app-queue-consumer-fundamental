edfc3af5b3f0d10855732ceac7ec6b84f2b395ba
sedanight <jerenawolunta@gmail.com>
2023-07-30 12:45:22 +0700

    first commit

---
+++ src/MailSender.js
@@ -0,0 +1,37 @@
+const nodemailer = require('nodemailer');
+ 
+class MailSender {
+  constructor() {
+    this._transporter = nodemailer.createTransport({
+      // host: 'smtp.gmail.com',
+      // port: 465,
+      // secure: true,
+      // auth: {
+      //   user: process.env.MAIL_ADDRESS,
+      //   pass: process.env.MAIL_PASSWORD,
+      // },
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASSWORD,
+        },
+    });
+  }
+  sendEmail(targetEmail, content) {
+    const message = {
+      from: 'Notes Apps',
+      to: targetEmail,
+      subject: 'Ekspor Catatan',
+      text: 'Terlampir hasil dari ekspor catatan',
+      attachments: [
+        {
+          filename: 'notes.json',
+          content,
+        },
+      ],
+    };
+    return this._transporter.sendMail(message);
+  }
+}
+module.exports = MailSender;
